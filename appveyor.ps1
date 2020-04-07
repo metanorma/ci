e7fb464e6df77cdc639020a0ee0bfcead0875a60
Alexande B <abobrikovich@gmail.com>
2020-04-07 14:49:25 +0300

    Remove unused CI configuration

--- appveyor.ps1
+++
@@ -1,5 +0,0 @@
-& choco install --no-progress plantuml make sed gnuwin32-coreutils.install grep
-& npm i -g puppeteer
-
-$RubyBin = "C:\Ruby${Env:RUBY_VERSION}\bin"
-& $RubyBin\gem.cmd install bundler
