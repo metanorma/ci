898aaf9f404fc16d762734e67c711a31b162ac40
Alexande B <abobrikovich@gmail.com>
2020-05-16 21:58:28 +0300

    metanorma/metanorma-docker#77 add script to install gems in docker

---
+++ gemfile-to-bundle-add.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+GEMFILE=${1:-./Gemfile}
+
+while IFS= read -r line; do
+	if [[ $line =~ ^gem ]]; then
+		GEM=$(echo "$line" | sed -e 's/gem[[:space:]]*//g' \
+			-e 's/[[:space:]]*,[[:space:]]*git:[[:space:]]*/ --git /g' \
+			-e 's/[[:space:]]*,[[:space:]]*branch:[[:space:]]*/ --branch /g')
+		bundle add "$GEM"
+	fi
+done < "$GEMFILE"
