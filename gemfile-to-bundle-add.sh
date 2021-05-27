74be040bdf415209acbeb5d128ff0a131fe4b361
Alexande B <abobrikovich@gmail.com>
2021-05-27 11:15:27 +0300

    Add source support for gemfile-to-bundle-add.sh script

--- gemfile-to-bundle-add.sh
+++ gemfile-to-bundle-add.sh
@@ -7,7 +7,8 @@ while IFS= read -r line; do
 	if [[ $line =~ ^gem ]]; then
 		GEM=$(echo "$line" | sed -e 's/gem[[:space:]]*//g' \
 			-e 's/[[:space:]]*,[[:space:]]*git:[[:space:]]*/ --git /g' \
-			-e 's/[[:space:]]*,[[:space:]]*branch:[[:space:]]*/ --branch /g')
+			-e 's/[[:space:]]*,[[:space:]]*branch:[[:space:]]*/ --branch /g' \
+			-e 's/[[:space:]]*,[[:space:]]*source:[[:space:]]*/ --source /g')
 		GEM=$(eval echo $GEM) # drop quotes
 		echo "> bundle add $GEM"
 		bundle add ${GEM}
