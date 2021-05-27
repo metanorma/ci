54479049d244e2e0f616c50cc74214bdd7ff1afa
Alexande B <abobrikovich@gmail.com>
2021-05-27 12:43:37 +0300

    Fix wrong sed 'gem ...' replacement

--- gemfile-to-bundle-add.sh
+++ gemfile-to-bundle-add.sh
@@ -5,7 +5,7 @@ GEMFILE=${1:-./Gemfile}
 
 while IFS= read -r line; do
 	if [[ $line =~ ^gem ]]; then
-		GEM=$(echo "$line" | sed -e 's/gem[[:space:]]*//g' \
+		GEM=$(echo "$line" | sed -e 's/^gem[[:space:]]*//g' \
 			-e 's/[[:space:]]*,[[:space:]]*git:[[:space:]]*/ --git /g' \
 			-e 's/[[:space:]]*,[[:space:]]*branch:[[:space:]]*/ --branch /g' \
 			-e 's/[[:space:]]*,[[:space:]]*source:[[:space:]]*/ --source /g')
