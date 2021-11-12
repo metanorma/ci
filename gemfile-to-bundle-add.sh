df42b02b20d622c53141cd4f8fb60e3ab4b9de63
Alexande B <abobrikovich@gmail.com>
2021-11-12 10:49:38 +0100

    #70 strip comments before Gemfile processing

--- gemfile-to-bundle-add.sh
+++ gemfile-to-bundle-add.sh
@@ -5,8 +5,8 @@ GEMFILE=${1:-./Gemfile}
 
 while IFS= read -r line; do
 	if [[ $line =~ ^gem ]]; then
-		GEM=$(echo "$line" | sed -e 's/^gem[[:space:]]*//g' \
-			-e 's/[[:space:]]*,[[:space:]]*git:[[:space:]]*/ --git /g' \
+		GEM=$(echo "$line" | cut -f1 -d"#" | sed -e 's/^gem[[:space:]]*//g' \
+			-e 's/[[:space:]]*,[[:space:]]*git:[[:space:]]*/ --git /g' 		\
 			-e 's/[[:space:]]*,[[:space:]]*branch:[[:space:]]*/ --branch /g' \
 			-e 's/[[:space:]]*,[[:space:]]*source:[[:space:]]*/ --source /g')
 		GEM=$(eval echo $GEM) # drop quotes
