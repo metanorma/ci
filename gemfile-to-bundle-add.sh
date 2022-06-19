580a06eb949e6ad7850ff376abfb48bbefaed912
Alexande B <abobrikovich@gmail.com>
2022-06-19 19:41:33 +0200

    Handle github param from Gemfile

--- gemfile-to-bundle-add.sh
+++ gemfile-to-bundle-add.sh
@@ -16,6 +16,7 @@ while IFS= read -r line; do
 	if [[ $line =~ ^gem ]]; then
 		GEM=$(echo "$line" | cut -f1 -d"#" | sed -e 's/^gem[[:space:]]*//g' \
 			-e "s/[[:space:]]*,[[:space:]]*git:[[:space:]]*[\"']\([A-Za-z0-9\:@\.\/\_-]*\)[\"']/ --git \1/g" 	   \
+			-e "s/[[:space:]]*,[[:space:]]*github:[[:space:]]*[\"']\([A-Za-z0-9\:@\.\/\_-]*\)[\"']/ --github \1/g" \
 			-e "s/[[:space:]]*,[[:space:]]*branch:[[:space:]]*[\"']\([A-Za-z0-9\:@\.\/\_-]*\)[\"']/ --branch \1/g" \
 			-e "s/[[:space:]]*,[[:space:]]*source:[[:space:]]*[\"']\([A-Za-z0-9\:@\.\/\_-]*\)[\"']/ --source \1/g" \
 			-e "s/[[:space:]]*,[[:space:]]*[\"']\([0-9a-z\.~>=<\-]*\)[\"']/ --version \1/g" )
