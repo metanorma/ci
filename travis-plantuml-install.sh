dd5a45a3ad73f187e14b7a3311f6d169f3c65a1b
Ronald Tse <ronald.tse@ribose.com>
2025-07-09 13:29:25 +0800

    chore: delete old obsolete files

--- travis-plantuml-install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-wget "http://downloads.sourceforge.net/project/plantuml/plantuml.jar?r=&ts=1424308684&use_mirror=jaist" -O plantuml.jar
-sudo mkdir -p /opt/plantuml
-sudo cp plantuml.jar /opt/plantuml
-echo '#!/usr/bin/env sh' > plantuml.sh
-echo 'exec java -jar /opt/plantuml/plantuml.jar "$@"' >> plantuml.sh
-sudo install -m 755 plantuml.sh /usr/local/bin/plantuml
-plantuml -version
